d27a808533a5164509a5a8a368f580efa2958618
Akshat Singh <akshats20@iiserb.ac.in>
2025-07-07 18:30:12 +0530

    added gamedev page

--- src/pages/random/Gamedev.tsx
+++ src/pages/random/Gamedev.tsx
@@ -1,7 +1,28 @@
+import './Gamedev.css'
+import Card from "../../components/Card";
+import Header_text from "../../components/Header_text";
+import Header_title from "../../components/Header_title";
+import Section from "../../components/Section";
+
 export default function Gamedev() {
     return(
         <>
-            <h1>Gamedev</h1>  
+            <Header_text>
+                    <Header_title>
+                        Games I have created which can be played on the web
+                    </Header_title>
+                </Header_text> 
+            <Section id="gamedev">
+                <div className="games-row">
+                    <Card
+                        href='jumpy_moo'
+                        img_src=''
+                        img_alt="Jumpy Moo"
+                        display_text="A google dino game clone. Made using Unity">
+                            Text
+                    </Card>
+                </div>
+            </Section> 
         </>
     )
 }
