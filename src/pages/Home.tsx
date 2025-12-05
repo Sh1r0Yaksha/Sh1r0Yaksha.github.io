7dd4d0d6bea6e0549f1681a36d1de342f8e6a503
Akshat Singh <akshats20@iiserb.ac.in>
2025-12-05 13:40:21 +0000

    Updated about section text

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -23,22 +23,14 @@ export default function Home() {
 
                     <div className="about">
                         <p>
-                            I am a final year BS-MS(Dual Degree) student at the
-                            Indian Institue of Science Education and Research(IISER) Bhopal,
-                            in the department of Electrical Engineering and
-                            Computer Science (EECS).<br />
-                            My Masters thesis focuses on Swarm Robotics and Virtual/Mixed
-                            Reality, exploring the interaction between humans and robots
-                            under the guidance of Prof. Sujit PB in the MOON lab at IISER Bhopal.
+                            I am a PhD researcher at Instituto Superior Técnico (IST), Lisbon Working in the Institute for Systems and Robotics (ISR) at the Department of Electronics and Computer Engineering (DEEC).<br />
+                            My Research focuses on the intersection of Extended Reality with Drone Swarms, exploring the various methods for human-swarm interaction which I am pursuing under the supervision of Dr. Meysam Basiri and Prof. Rodrigo Ventura.<br />
+                            This research work is an extension of my Masters thesis which focused on Swarm Robotics and Virtual/Mixed Reality which I pursued under the guidance of Prof. Sujit PB in the MOON lab at IISER Bhopal, where I also completed my bachelors and masters as a 5-year dual-degree.
                         </p>
                         <p>
-                            Beyond academics, I am passionate about new technologies and
-                            strive to learn about any new piece of technology I encounter.<br />
-                            I enjoy anime, movies, and video games, with a passion for
-                            speedrunning, where I hold world records in a few games.
-                            I also like reading and traveling, which broaden my perspectives.<br />
-                            In my free time, I work on small coding projects to
-                            enhance my skills.
+                            Beyond academics, I am passionate about new technologies and strive to learn about any new piece of technology I encounter.<br />
+                            I enjoy anime, movies, and video games, with a passion for speedrunning, where I hold world records in a few games. I also like reading and traveling, and have started to cook as a hobby, which broaden my perspectives.<br />
+                            In my free time, I work on small coding projects to enhance my skills.
                         </p>
                     </div>
                     <div className="CV">
